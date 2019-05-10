37c25167b07b833611240c956da5be2c57519d37
pkandathil <prashantgeorge36@gmail.com>
2019-05-10 15:35:20 -0700

    Solution for recursion

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fibonacci = (inputNumber) => {
   if (inputNumber === 0 || inputNumber === 1) {
     return inputNumber
   }
+  const result = fibonacci(inputNumber - 1) + fibonacci(inputNumber - 2)
+  return result
 }
 
 module.exports = {
--- test/app.spec.js
+++ test/app.spec.js
@@ -11,4 +11,14 @@ describe('Fibonacci', () => {
     expect(result).toEqual(1)
     done()
   })
+  it('f(3) = 5', (done) => {
+    const result = fibonacci(3)
+    expect(result).toEqual(2)
+    done()
+  })
+  it('f(5) = 5', (done) => {
+    const result = fibonacci(5)
+    expect(result).toEqual(5)
+    done()
+  })
 })
