c562207c4ab6c182a2447c44f6198513c6f0a72f
pkandathil <prashantgeorge36@gmail.com>
2019-05-14 10:36:16 -0700

    Unrolled loop solution

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const fibonacci = (inputNumber) => {
   if (inputNumber === 0 || inputNumber === 1) {
     return inputNumber
   }
+  const resultContainer = {
+    0: 0,
+    1: 1
+  }
+  for(let index = 0; index <= inputNumber; index++) {
+    if(index in resultContainer) {
+      continue;
+    } else {
+      resultContainer[index] = resultContainer[index-1] + resultContainer[index - 2]
+    }
+  }
+  return resultContainer[inputNumber]
 }
 
 module.exports = {
--- test/app.spec.js
+++ test/app.spec.js
@@ -11,4 +11,24 @@ describe('Fibonacci', () => {
     expect(result).toEqual(1)
     done()
   })
+  it('f(3) = 2', (done) => {
+    const result = fibonacci(3)
+    expect(result).toEqual(2)
+    done()
+  })
+  it('f(5) = 5', (done) => {
+    const result = fibonacci(5)
+    expect(result).toEqual(5)
+    done()
+  })
+  it('f(10) = 55', (done) => {
+    const result = fibonacci(10)
+    expect(result).toEqual(55)
+    done()
+  })
+  it('f(99) = 218922995834555169026', (done) => {
+    const result = fibonacci(99)
+    expect(result).toEqual(218922995834555169026)
+    done()
+  })
 })
